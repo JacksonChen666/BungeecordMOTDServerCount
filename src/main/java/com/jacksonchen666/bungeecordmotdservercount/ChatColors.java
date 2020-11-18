886cf56c90add49855a8f9946bc1b90246c672f9
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-11-18 22:44:45 +0100

    added this thing for use

---
+++ src/main/java/com/jacksonchen666/bungeecordmotdservercount/ChatColors.java
@@ -0,0 +1,9 @@
+package com.jacksonchen666.bungeecordmotdservercount;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class ChatColors {
+    public static String color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+}
