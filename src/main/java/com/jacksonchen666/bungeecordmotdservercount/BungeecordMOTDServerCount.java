bc52cbd9520fea4d0d76ddc101a3c4089e9d2e18
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-11-18 23:27:42 +0100

    able to get percentage colors from config

--- src/main/java/com/jacksonchen666/bungeecordmotdservercount/BungeecordMOTDServerCount.java
+++ src/main/java/com/jacksonchen666/bungeecordmotdservercount/BungeecordMOTDServerCount.java
@@ -1,5 +1,6 @@
 package com.jacksonchen666.bungeecordmotdservercount;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
 import net.md_5.bungee.config.Configuration;
@@ -10,8 +11,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BungeecordMOTDServerCount extends Plugin {
+    private final Map<Integer, Map<Integer, String>> colorsConfig = new HashMap<>();
     private Configuration config;
     private ServerCountManager countManager;
 
@@ -19,6 +25,24 @@ public class BungeecordMOTDServerCount extends Plugin {
     public void onLoad() {
         saveDefaultConfig();
         config = loadConfig();
+        Configuration tempColorConfig = config.getSection("settings.colors");
+        List<String> startings = tempColorConfig.getKeys().stream().filter(item -> item.contains("percent_")).collect(Collectors.toList());
+        for (String start : startings) {
+            String color = tempColorConfig.getString(start + ".color");
+            try {
+                colorsConfig.put(Integer.parseInt(start.replace("percent_", "")), new HashMap<Integer, String>() {{
+                    put(tempColorConfig.getInt(start + ".end"), color.contains("&") && color.length() == 2 ? color : ChatColor.valueOf(color.toUpperCase()).toString());
+                }});
+            }
+            catch (IllegalArgumentException e) {
+                if (e.getMessage().equals("No enum constant net.md_5.bungee.api.ChatColor." + color)) {
+                    System.err.println("Tried to get color from \"" + start + "\" but instead got \"" + color + "\" which does not exist.");
+                }
+                else {
+                    throw e;
+                }
+            }
+        }
     }
 
     @Override
@@ -31,6 +55,7 @@ public class BungeecordMOTDServerCount extends Plugin {
     @Override
     public void onDisable() {
         saveConfig();
+        colorsConfig.clear();
     }
 
     private void saveDefaultConfig() {
@@ -75,5 +100,9 @@ public class BungeecordMOTDServerCount extends Plugin {
 
     Configuration getConfig() {
         return config;
+    }
+
+    public Map<Integer, Map<Integer, String>> getColorsConfig() {
+        return colorsConfig;
     }
 }
